d94a59d90e353603bfd59372f9c0c40672c4450d
Oncu <oohancan@gmail.com>
2022-08-10 20:05:11 +0300

    add SearchBar

---
+++ NotesApp/SearchBar.swift
@@ -0,0 +1,37 @@
+//
+//  SearchBar.swift
+//  NotesApp
+//
+//  Created by Oncu Can on 10.08.2022.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    @Binding var text: String
+    
+    var body: some View {
+        HStack {
+            TextField("Search", text: $text)
+                .padding(8)
+                .padding(.horizontal, 24)
+                .background(Color(.systemGray6)).foregroundColor(.gray)
+                .cornerRadius(8)
+                .overlay(
+                    Image(systemName: "magnifyingglass")
+                    .foregroundColor(.gray)
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 8)
+
+                )
+            
+            
+        }
+        .padding(.horizontal, 10)
+    }
+}
+struct SearchBar_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBar(text: .constant("Search..."))
+    }
+}
