cb6b6508971afc3ce0beb17ec62bb12359086917
Oncu <oohancan@gmail.com>
2022-08-10 19:39:56 +0300

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by Oncu Can on 10.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
