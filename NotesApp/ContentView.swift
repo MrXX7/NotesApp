ae3bf0119c16f9a71fe15ac7fa756df7dd4d795e
Oncu <oohancan@gmail.com>
2022-08-11 10:01:37 +0300

    add NoteView and edit SearchBar

--- NotesApp/ContentView.swift
+++ NotesApp/ContentView.swift
@@ -9,8 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                SearchBar()
+                Spacer()
+                List {
+                    NavigationLink("1", destination: NoteView())
+                }
+                .listStyle(.inset)
+            }
+            .navigationTitle("Notes")
+        }
     }
 }
 
---
+++ NotesApp/NoteView.swift
@@ -0,0 +1,19 @@
+//
+//  NoteView.swift
+//  NotesApp
+//
+//  Created by Oncu Can on 10.08.2022.
+//
+
+import SwiftUI
+
+struct NoteView: View {
+    var body: some View {
+        Text("note")
+    }
+}
+struct NoteView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoteView()
+    }
+}
